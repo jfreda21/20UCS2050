4248d161095bfa3e3e3a681c4b62d8b9da2112d1
Thyago Mota <tmota@msudenver.edu>
2020-05-27 11:17:47 -0600

    activity_01

---
+++ activity_01_bsheep/src/BSheep.java
@@ -0,0 +1,17 @@
+/*
+ * CS2050 - Computer Science II - Summer 2020
+ * Instructor: Thyago Mota
+ * Description: Activity 01 - BSheep
+ */
+
+import java.util.Scanner;
+
+public class BSheep {
+
+    static final int MIN_NUM_SHEEP = 2;
+
+    public static void main(String[] args) {
+
+        
+    }
+} // end of class definition
