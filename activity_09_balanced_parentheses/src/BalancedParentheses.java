85c354a69cf79e400ee8992b4c585ba9a6ff9bc0
Thyago Mota <tmota@msudenver.edu>
2020-06-12 15:32:26 -0600

    updates

---
+++ activity_09_balanced_parentheses/src/BalancedParentheses.java
@@ -0,0 +1,26 @@
+/*
+ * CS2050 - Computer Science II - Summer 2020
+ * Instructor: Thyago Mota
+ * Description: Activity 09 - BalancedParentheses
+ */
+
+import java.util.Scanner;
+
+public class BalancedParentheses {
+
+    // TODO: implement isBalanced using a stack
+    public static boolean isBalanced(String exp) {
+       return false; // placeholder
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("? ");
+        String exp = in.nextLine();
+        System.out.print("The parentheses in expression \"" + exp + "\" are ");
+        if (isBalanced(exp))
+            System.out.println("balanced!");
+        else
+            System.out.println("not balanced!");
+    }
+}
